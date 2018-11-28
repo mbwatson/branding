5bf1b1f4cbe394c7ac4d6ae7c3698808c947996b
mbwatson <mattwt@gmail.com>
2018-11-28 13:09:44 -0500

    minimal working groups dropdown

--- src/app/Menu.js
+++ src/app/Menu.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import { NavLink } from 'react-router-dom'
-import { List, ListSubheader, ListItem, ListItemIcon, ListItemText } from '@material-ui/core'
+import { List, ListSubheader, ListItem, ListItemIcon, ListItemText, Collapse } from '@material-ui/core'
 import {
     Dashboard as DashboardIcon,
     Face as FaceIcon,
@@ -10,6 +10,9 @@ import {
     TextFields as FontsIcon,
     KeyboardArrowRight as KeyboardArrowRightIcon,
     GroupWork as GroupWorkIcon,
+    Inbox as InboxIcon,
+    ExpandLess,
+    ExpandMore,
 } from '@material-ui/icons'
 
 const styles = (theme) => ({
@@ -26,34 +29,69 @@ const mainMenuItems = [
     { text: 'Groups', path: '/groups', icon: <GroupWorkIcon/> },
 ]
 
-// const groupsMenuItems = [
-//     { text: 'iRODS', path: '/groups/irods', icon: <KeyboardArrowRightIcon/> },
-//     { text: 'NCDS', path: '/groups/ncds', icon: <KeyboardArrowRightIcon/> },
-//     { text: 'Hydroshare', path: '/groups/hydroshare', icon: <KeyboardArrowRightIcon/> },
-// ]
+const groupsMenuItems = [
+    { text: 'iRODS', path: '/groups/irods', icon: <KeyboardArrowRightIcon/> },
+    { text: 'NCDS', path: '/groups/ncds', icon: <KeyboardArrowRightIcon/> },
+    { text: 'Hydroshare', path: '/groups/hydroshare', icon: <KeyboardArrowRightIcon/> },
+]
 
-const menu = props => {
-    const { classes } = props
-    return (
-        <div>
-            <List subheader={<ListSubheader component="div">RENCI</ListSubheader>}>
-                {
-                    mainMenuItems.map( (item) => {
-                        return (
-                            <ListItem button component={ NavLink } to={ item.path }
-                                key={ item.text } activeClassName={ classes.active } exact
-                            >
-                                <ListItemIcon>
-                                    { item.icon }
-                                </ListItemIcon>
-                                <ListItemText primary={ item.text } />
-                            </ListItem>
-                        )
-                    })
-                }
-            </List>
-        </div>
-    )
+class menu extends Component {
+    state = {
+        open: true,
+    }
+
+    clickHandler = () => {
+        this.setState(state => ({ open: !state.open }))
+    }
+
+    render() {
+        const { classes } = this.props
+        return (
+            <div>
+                <List subheader={<ListSubheader component="div">RENCI</ListSubheader>}>
+                    {
+                        mainMenuItems.map( (item) => {
+                            return (
+                                <ListItem button component={ NavLink } to={ item.path }
+                                    key={ item.text } activeClassName={ classes.active } exact
+                                >
+                                    <ListItemIcon>
+                                        { item.icon }
+                                    </ListItemIcon>
+                                    <ListItemText primary={ item.text } />
+                                </ListItem>
+                            )
+                        })
+                    }
+                    <ListItem button onClick={ this.clickHandler }>
+                        <ListItemIcon>
+                            <InboxIcon />
+                        </ListItemIcon>
+                        <ListItemText inset primary="Inbox" />
+                        { this.state.open ? <ExpandLess /> : <ExpandMore /> }
+                    </ListItem>
+                </List>
+                <Collapse in={ this.state.open } timeout="auto" unmountOnExit>
+                    <List component="div">
+                        {
+                            groupsMenuItems.map( (item) => {
+                                return (
+                                    <ListItem button component={ NavLink } to={ item.path }
+                                        key={ item.text } activeClassName={ classes.active } exact
+                                    >
+                                        <ListItemIcon>
+                                            { item.icon }
+                                        </ListItemIcon>
+                                        <ListItemText primary={ item.text } />
+                                    </ListItem>
+                                )
+                            })
+                        }
+                    </List>
+                </Collapse>
+            </div>
+        )        
+    }
 }
 
 export default withStyles(styles)(menu)
